f2b1bef88a899eb32c3f1d4b9e036efdbd594769
NatashaElliott <natashayelliott@gmail.com>
2024-11-27 17:24:42 +1100

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ function buildMetadata(sample) {
 	let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-	let sample_number = metadata.filter(meta => meta.id == sample);
+	let sample_number = metadata.find(meta => meta.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
 	let panel = d3.select("#sample-metadata");
